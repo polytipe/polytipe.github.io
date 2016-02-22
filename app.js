9b251cdc022c941af4e6171401b958c30469a5a0
alejost848 <alejost848@gmail.com>
2016-02-21 23:03:41 -0500

    poly-tabs bindings are working

--- app.js
+++ app.js
@@ -19,60 +19,86 @@ function iframe_ready() {
 
     for (var key in element_properties) {
       //if (!key.startsWith("_")) { //Hide private properties
-        if (element_properties[key].type.name == 'Array') {
-          //Title of property
-          var div = document.createElement("div");
-          div.innerHTML = key;
-          div.classList.add("property_name");
-          properties_list.appendChild(div);
+      if (element_properties[key].type.name == 'Array') {
+        //Title of property
+        var div = document.createElement("div");
 
-          var array_elements = selected_element[key];
-          for (var i = 0; i < array_elements.length; i++) {
-            var input = document.createElement("paper-input");
-            input.label = (i + 1);
-            input.id = key + ("0" + (i + 1)).slice(-2); //00 01 formatting
-            for (var pass in array_elements[i]) {
-               input.value = array_elements[i][pass];
-            }
-            input.addEventListener("change", propertyChanged);
-            input.classList.add("sub_property");
-            properties_list.appendChild(input);
-          }
-        } else if (element_properties[key].type.name == 'Boolean') {
-          //Title of property
-          var div = document.createElement("div");
-          var innerDiv = document.createElement("div");
-          innerDiv.innerHTML = key;
-          innerDiv.classList.add("flex");
-          div.classList.add("property_name", "layout", "horizontal");
-          div.appendChild(innerDiv);
+        var innerDiv = document.createElement("div");
+        innerDiv.innerHTML = key;
+        innerDiv.classList.add("flex");
+        div.classList.add("property_name", "layout", "horizontal");
+        div.appendChild(innerDiv);
 
-          var input = document.createElement("paper-toggle-button");
-          if (element_properties[key].value == true) {
-            input.checked = true;
-          } else {
-            input.checked = false;
-          }
-          input.id = key;
-          input.addEventListener("change", propertyChanged);
-          div.appendChild(input);
-          properties_list.appendChild(div);
-        } else if (element_properties[key].type.name == 'Number') {
+        var addButton = document.createElement("paper-fab");
+        addButton.icon = "add";
+        addButton.title = key;
+        addButton.mini = true;
+        addButton.classList.add("array_fab");
+        addButton.addEventListener("click", arrayChanged);
+
+        div.appendChild(addButton);
+
+        var removeButton = document.createElement("paper-fab");
+        removeButton.icon = "remove";
+        removeButton.title = key;
+        removeButton.mini = true;
+        removeButton.classList.add("array_fab");
+        removeButton.addEventListener("click", arrayChanged);
+
+        div.appendChild(removeButton);
+
+        properties_list.appendChild(div);
+
+        var array_elements = selected_element[key];
+        for (var i = 0; i < array_elements.length; i++) {
           var input = document.createElement("paper-input");
-          input.label = key;
-          input.id = key;
-          input.type = "number";
-          input.addEventListener("change", propertyChanged);
-          input.min = selected_element.min;
-          input.max = selected_element.max;
-
-          if (key.startsWith("selected")) { //Prevent first position being 0
-            input.value = selected_element[key] + 1;
-          } else {
-            input.value = selected_element[key];
+          input.label = (i + 1);
+          input.id = key + ("0" + (i + 1)).slice(-2); //00 01 formatting
+          for (var pass in array_elements[i]) {
+            input.value = array_elements[i][pass];
           }
+          input.addEventListener("change", propertyChanged);
+          input.classList.add("sub_property");
           properties_list.appendChild(input);
         }
+
+
+
+      } else if (element_properties[key].type.name == 'Boolean') {
+        //Title of property
+        var div = document.createElement("div");
+        var innerDiv = document.createElement("div");
+        innerDiv.innerHTML = key;
+        innerDiv.classList.add("flex");
+        div.classList.add("property_name", "layout", "horizontal");
+        div.appendChild(innerDiv);
+
+        var input = document.createElement("paper-toggle-button");
+        if (element_properties[key].value == true) {
+          input.checked = true;
+        } else {
+          input.checked = false;
+        }
+        input.id = key;
+        input.addEventListener("change", propertyChanged);
+        div.appendChild(input);
+        properties_list.appendChild(div);
+      } else if (element_properties[key].type.name == 'Number') {
+        var input = document.createElement("paper-input");
+        input.label = key;
+        input.id = key;
+        input.type = "number";
+        input.addEventListener("change", propertyChanged);
+        input.min = selected_element.min;
+        input.max = selected_element.max;
+
+        if (key.startsWith("selected")) { //Prevent first position being 0
+          input.value = selected_element[key] + 1;
+        } else {
+          input.value = selected_element[key];
+        }
+        properties_list.appendChild(input);
+      }
       //}
     }
 
@@ -102,36 +128,44 @@ function unfocus(e) {
 function propertyChanged() {
   //if (!(this.id).startsWith("_")) { //Hide private properties
 
-    var arrayName;
-    if(/\d/.test(this.id)){ //If the property name has a number (array)
-      arrayName = (this.id).substr(0, (this.id).length-2);
-    }else{
-      arrayName = this.id;
-    }
+  var arrayName;
+  if (/\d/.test(this.id)) { //If the property name has a number (array)
+    arrayName = (this.id).substr(0, (this.id).length - 2);
+  } else {
+    arrayName = this.id;
+  }
 
-    if (element_properties[arrayName].type.name == 'Array') {
-      var jsonArray = [];
-      for (var i = 0; i < selected_element[arrayName].length; i++) {
-        var jsonObj = {};
-        var obj = selected_element[arrayName][i];
-        for (var key in obj) {
-           jsonObj[key] = document.getElementById(arrayName + ("0" + (i + 1)).slice(-2)).value; //00 01 formatting
-        }
-        jsonArray.push(jsonObj);
+  if (element_properties[arrayName].type.name == 'Array') {
+    var jsonArray = [];
+    for (var i = 0; i < selected_element[arrayName].length; i++) {
+      var jsonObj = {};
+      var obj = selected_element[arrayName][i];
+      for (var key in obj) {
+        jsonObj[key] = document.getElementById(arrayName + ("0" + (i + 1)).slice(-2)).value; //00 01 formatting
       }
-      selected_element[arrayName] = jsonArray;
+      jsonArray.push(jsonObj);
     }
-    else if (element_properties[this.id].type.name == 'Boolean') {
-      console.log("TODO: boolean property");
+    selected_element[arrayName] = jsonArray;
+  } else if (element_properties[this.id].type.name == 'Boolean') {
+    console.log("TODO: boolean property");
+  } else if (element_properties[this.id].type.name == 'Number') {
+    if (this.id.startsWith("selected")) { //Prevent first position being 0
+      selected_element[this.id] = parseInt(this.value) - 1;
+    } else {
+      selected_element[this.id] = parseInt(this.value);
     }
-    else if (element_properties[this.id].type.name == 'Number') {
-      if (this.id.startsWith("selected")) { //Prevent first position being 0
-        selected_element[this.id] = parseInt(this.value) - 1;
-      } else {
-        selected_element[this.id] = parseInt(this.value);
-      }
-    }
+  }
   //}
+}
+
+function arrayChanged() {
+
+  if (this.icon == "add") {
+    selected_element.addTab();
+  }
+  if (this.icon == "remove") {
+    selected_element.removeTab();
+  }
 }
 
 function makeElement(element_name) {
