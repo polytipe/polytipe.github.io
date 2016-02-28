48a6c1db9d05b9f397470b2d6ee5ce355f7852af
alejost848 <alejost848@gmail.com>
2016-02-27 19:48:02 -0500

    Create dropdown inputs for poly-layout with working bindings

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 window.addEventListener('WebComponentsReady', function(e) {
+
 });
 
 var app_sidebar = document.querySelector("#app_sidebar");
 app_sidebar.selected_mode = 0;
 
 var selected_element;
+var selected_layout_frame;
 var element_properties;
 
 function iframe_ready() {
@@ -13,106 +15,135 @@ function iframe_ready() {
 
   var properties_list = document.getElementById('properties_list');
 
-  iframe_document.addEventListener('regularTap', function(e) {
+  iframe_document.addEventListener('elementSelection', function(e) {
     selected_element = e.target;
     element_properties = selected_element.properties;
 
     //Unfocus all elements except the selected_element
     unfocus(e);
 
-    for (var key in element_properties) {
-      //if (!key.startsWith("_")) { //Hide private properties
-      if (element_properties[key].type.name == 'Array') {
-        //Title of property
-        var div = document.createElement("div");
+    //Add inputs for poly-layout elements
+    if(selected_element.tagName == "POLY-LAYOUT"){
+      for (var key in element_properties) {
+        if (element_properties[key].type.name == 'String') {
+          //Create dropdowns for every String property
+          var dropdown = document.createElement("layout-dropdown");
+          dropdown.label = key;
+          dropdown.id = key;
+          dropdown.value = selected_element[key];
+          dropdown.items = selected_element[key+"Dropdown"];
+          dropdown.selectedIndex = dropdown.items.indexOf(dropdown.value); //Get index of selected item
+          dropdown.addEventListener("iron-select", propertyChanged);
+          properties_list.appendChild(dropdown);
+        }else if (element_properties[key].type.name == 'Boolean') {
+          //Title of property
+          var div = document.createElement("div");
+          var innerDiv = document.createElement("div");
+          innerDiv.innerHTML = key;
+          innerDiv.classList.add("flex");
+          div.classList.add("property_name", "layout", "horizontal");
+          div.appendChild(innerDiv);
 
-        var innerDiv = document.createElement("div");
-        innerDiv.innerHTML = key;
-        innerDiv.classList.add("flex");
-        div.classList.add("property_name", "layout", "horizontal");
-        div.appendChild(innerDiv);
-
-        var addButton = document.createElement("paper-fab");
-        addButton.icon = "add";
-        addButton.title = key;
-        addButton.mini = true;
-        addButton.classList.add("array_fab");
-        addButton.addEventListener("click", arrayChanged);
-
-        div.appendChild(addButton);
-
-        var removeButton = document.createElement("paper-fab");
-        removeButton.icon = "remove";
-        removeButton.title = key;
-        removeButton.mini = true;
-        removeButton.classList.add("array_fab");
-        removeButton.addEventListener("click", arrayChanged);
-
-        div.appendChild(removeButton);
-
-        properties_list.appendChild(div);
-
-        var array_elements = selected_element[key];
-        for (var i = 0; i < array_elements.length; i++) {
-          var input = document.createElement("paper-input");
-          input.label = (i + 1);
-          input.id = key + ("0" + (i + 1)).slice(-2); //00 01 formatting
-          for (var pass in array_elements[i]) {
-            input.value = array_elements[i][pass];
+          var input = document.createElement("paper-toggle-button");
+          input.classList.add("layout", "vertical", "end");
+          if (element_properties[key].value == true) {
+            input.checked = true;
+          } else {
+            input.checked = false;
           }
+          input.id = key;
           input.addEventListener("change", propertyChanged);
-          input.classList.add("sub_property");
+          div.appendChild(input);
+          properties_list.appendChild(div);
+        }
+      }
+    } else{ //Add inputs for all other elements
+      for (var key in element_properties) {
+        if (element_properties[key].type.name == 'Array') {
+          //Title of property
+          var div = document.createElement("div");
+
+          var innerDiv = document.createElement("div");
+          innerDiv.innerHTML = key;
+          innerDiv.classList.add("flex");
+          div.classList.add("property_name", "layout", "horizontal");
+          div.appendChild(innerDiv);
+
+          var addButton = document.createElement("paper-fab");
+          addButton.icon = "add";
+          addButton.title = key;
+          addButton.mini = true;
+          addButton.classList.add("array_fab");
+          addButton.addEventListener("click", arrayChanged);
+          div.appendChild(addButton);
+
+          var removeButton = document.createElement("paper-fab");
+          removeButton.icon = "remove";
+          removeButton.title = key;
+          removeButton.mini = true;
+          removeButton.classList.add("array_fab");
+          removeButton.addEventListener("click", arrayChanged);
+          div.appendChild(removeButton);
+
+          properties_list.appendChild(div);
+
+          var array_elements = selected_element[key];
+          for (var i = 0; i < array_elements.length; i++) {
+            var input = document.createElement("paper-input");
+            input.label = (i + 1);
+            input.id = key + ("0" + (i + 1)).slice(-2); //00 01 formatting
+            for (var pass in array_elements[i]) {
+              input.value = array_elements[i][pass];
+            }
+            input.addEventListener("change", propertyChanged);
+            input.classList.add("sub_property");
+            properties_list.appendChild(input);
+          }
+        } else if (element_properties[key].type.name == 'Boolean') {
+          //Title of property
+          var div = document.createElement("div");
+          var innerDiv = document.createElement("div");
+          innerDiv.innerHTML = key;
+          innerDiv.classList.add("flex");
+          div.classList.add("property_name", "layout", "horizontal");
+          div.appendChild(innerDiv);
+
+          var input = document.createElement("paper-toggle-button");
+          if (element_properties[key].value == true) {
+            input.checked = true;
+          } else {
+            input.checked = false;
+          }
+          input.id = key;
+          input.addEventListener("change", propertyChanged);
+          div.appendChild(input);
+          properties_list.appendChild(div);
+        } else if (element_properties[key].type.name == 'Number') {
+          var input = document.createElement("paper-input");
+          input.label = key;
+          input.id = key;
+          input.type = "number";
+          input.addEventListener("change", propertyChanged);
+          input.min = selected_element.min;
+          input.max = selected_element.max;
+
+          if (key.startsWith("selected")) { //Prevent first position being 0
+            input.value = selected_element[key] + 1;
+          } else {
+            input.value = selected_element[key];
+          }
+          properties_list.appendChild(input);
+        } else if (element_properties[key].type.name == 'String') {
+          var input = document.createElement("paper-input");
+          input.label = key;
+          input.id = key;
+          input.type = "text";
+          input.addEventListener("change", propertyChanged);
+          input.value = selected_element[key];
           properties_list.appendChild(input);
         }
-
-
-
-      } else if (element_properties[key].type.name == 'Boolean') {
-        //Title of property
-        var div = document.createElement("div");
-        var innerDiv = document.createElement("div");
-        innerDiv.innerHTML = key;
-        innerDiv.classList.add("flex");
-        div.classList.add("property_name", "layout", "horizontal");
-        div.appendChild(innerDiv);
-
-        var input = document.createElement("paper-toggle-button");
-        if (element_properties[key].value == true) {
-          input.checked = true;
-        } else {
-          input.checked = false;
-        }
-        input.id = key;
-        input.addEventListener("change", propertyChanged);
-        div.appendChild(input);
-        properties_list.appendChild(div);
-      } else if (element_properties[key].type.name == 'Number') {
-        var input = document.createElement("paper-input");
-        input.label = key;
-        input.id = key;
-        input.type = "number";
-        input.addEventListener("change", propertyChanged);
-        input.min = selected_element.min;
-        input.max = selected_element.max;
-
-        if (key.startsWith("selected")) { //Prevent first position being 0
-          input.value = selected_element[key] + 1;
-        } else {
-          input.value = selected_element[key];
-        }
-        properties_list.appendChild(input);
-      } else if (element_properties[key].type.name == 'String') {
-        var input = document.createElement("paper-input");
-        input.label = key;
-        input.id = key;
-        input.type = "text";
-        input.addEventListener("change", propertyChanged);
-        input.value = selected_element[key];
-        properties_list.appendChild(input);
       }
-      //}
     }
-
   });
 }
 
@@ -134,11 +165,8 @@ function unfocus(e) {
   }
 }
 
-
 //Update property values using the value of the inputs
 function propertyChanged() {
-  //if (!(this.id).startsWith("_")) { //Hide private properties
-
   var arrayName;
   if (/\d/.test(this.id)) { //If the property name has a number (array)
     arrayName = (this.id).substr(0, (this.id).length - 2);
@@ -168,11 +196,9 @@ function propertyChanged() {
   } else if (element_properties[this.id].type.name == 'String') {
     selected_element[this.id] = this.value;
   }
-  //}
 }
 
 function arrayChanged() {
-
   if (this.icon == "add") {
     selected_element.addTab();
   }
